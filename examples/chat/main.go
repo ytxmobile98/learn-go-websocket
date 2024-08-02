795945b512fbd9b05577225daf6034c9740e4285
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2024-08-02 21:22:40 +0800

    feat: added websocket endpoint

--- examples/chat/main.go
+++ examples/chat/main.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+)
+
+const (
+	defaultPort uint16 = 8080
 )
 
 func main() {
 	flag.Parse()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world!"))
-	})
+	http.HandleFunc("/", serveRoot)
+	http.HandleFunc("/ws", serveWs)
 
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", defaultPort), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func serveRoot(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world!"))
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("WebSocket endpoint"))
 }
