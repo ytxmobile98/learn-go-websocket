9d69d3c9bcbf69ed2a3cdf89dc712a61b784c4fd
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2024-08-03 23:20:39 +0800

    feat: client & server

--- examples/echo/client/main.go
+++ examples/echo/client/main.go
@@ -3,9 +3,91 @@ package main
 import (
 	"flag"
 	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
 
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
+	urlStr := u.String()
+	log.Printf("Connecting to %s", urlStr)
+
+	dialer := websocket.DefaultDialer
+	conn, _, err := dialer.Dial(urlStr, nil)
+	if err != nil {
+		log.Fatal("Dial:", err)
+	}
+	defer conn.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+
+		for {
+			messageType, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("Read:", err)
+				return
+			}
+			log.Printf("Received [%d]: %s", messageType, message)
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-done:
+			return
+		case t := <-ticker.C:
+			if err := writeMessage(conn, []byte(t.String())); err != nil {
+				return
+			}
+		case <-interrupt:
+			interruptConn(conn, done)
+			return
+		}
+	}
+}
+
+func writeMessage(conn *websocket.Conn, message []byte) error {
+	err := conn.WriteMessage(websocket.TextMessage, message)
+	if err != nil {
+		log.Println("Write:", err)
+		return err
+	}
+	return nil
+}
+
+func interruptConn(conn *websocket.Conn, done chan struct{}) error {
+	log.Println("Interrupt")
+
+	// Cleanly close the connection by sending a close message and then
+	// waiting (with timeout) for the server to close the connection.
+	err := conn.WriteMessage(websocket.CloseMessage,
+		websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	if err != nil {
+		log.Println("Write close:", err)
+		return err
+	}
+
+	select {
+	case <-done:
+	case <-time.After(time.Second):
+	}
+	return nil
 }
--- examples/echo/server/main.go
+++ examples/echo/server/main.go
@@ -2,10 +2,143 @@ package main
 
 import (
 	"flag"
+	"html/template"
 	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	addr = flag.String("addr", "localhost:8080", "http service address")
+
+	upgrader = websocket.Upgrader{} // use efault options
 )
 
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
+
+	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/", home)
+
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("upgrade:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		if err := processMessage(conn); err != nil {
+			break
+		}
+	}
+}
+
+func processMessage(conn *websocket.Conn) error {
+	// read message
+	messageType, message, err := conn.ReadMessage()
+	if err != nil {
+		log.Println("read:", err)
+		return err
+	}
+	log.Printf("received: [%d] %s", messageType, message)
+
+	// write message
+	err = conn.WriteMessage(messageType, message)
+	if err != nil {
+		log.Println("write:", err)
+		return err
+	}
+
+	return nil
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
+}
+
+var homeTemplate = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script>
+window.addEventListener("load", function(evt) {
+
+    var output = document.getElementById("output");
+    var input = document.getElementById("input");
+    var ws;
+
+    var print = function(message) {
+        var d = document.createElement("div");
+        d.textContent = message;
+        output.appendChild(d);
+        output.scroll(0, output.scrollHeight);
+    };
+
+    document.getElementById("open").onclick = function(evt) {
+        if (ws) {
+            return false;
+        }
+        ws = new WebSocket("{{.}}");
+        ws.onopen = function(evt) {
+            print("OPEN");
+        }
+        ws.onclose = function(evt) {
+            print("CLOSE");
+            ws = null;
+        }
+        ws.onmessage = function(evt) {
+            print("RESPONSE: " + evt.data);
+        }
+        ws.onerror = function(evt) {
+            print("ERROR: " + evt.data);
+        }
+        return false;
+    };
+
+    document.getElementById("send").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        print("SEND: " + input.value);
+        ws.send(input.value);
+        return false;
+    };
+
+    document.getElementById("close").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        ws.close();
+        return false;
+    };
+
+});
+</script>
+</head>
+<body>
+<table>
+<tr><td valign="top" width="50%">
+<p>Click "Open" to create a connection to the server,
+"Send" to send a message to the server and "Close" to close the connection.
+You can change the message and send multiple times.
+<p>
+<form>
+<button id="open">Open</button>
+<button id="close">Close</button>
+<p><input id="input" type="text" value="Hello world!">
+<button id="send">Send</button>
+</form>
+</td><td valign="top" width="50%">
+<div id="output" style="max-height: 70vh;overflow-y: scroll;"></div>
+</td></tr></table>
+</body>
+</html>
+`))
