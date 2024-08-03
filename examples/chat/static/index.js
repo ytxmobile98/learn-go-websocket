01d146f342391b0b0b2cd51f4ae4b1f3818f4576
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2024-08-03 10:39:36 +0800

    style: use `let` and `const` in js script

--- examples/chat/static/index.js
+++ examples/chat/static/index.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', function() {
-    var conn;
-    var msg = document.getElementById("msg");
-    var log = document.getElementById("log");
+    let conn;
+    const msg = document.getElementById("msg");
+    const log = document.getElementById("log");
 
     function appendLog(item) {
-        var doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
+        const doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
         log.appendChild(item);
         if (doScroll) {
             log.scrollTop = log.scrollHeight - log.clientHeight;
@@ -26,20 +26,20 @@ window.addEventListener('load', function() {
     if (window["WebSocket"]) {
         conn = new WebSocket("ws://" + document.location.host + "/ws");
         conn.onclose = function (evt) {
-            var item = document.createElement("div");
+            const item = document.createElement("div");
             item.innerHTML = "<b>Connection closed.</b>";
             appendLog(item);
         };
         conn.onmessage = function (evt) {
-            var messages = evt.data.split('\n');
-            for (var i = 0; i < messages.length; i++) {
-                var item = document.createElement("div");
-                item.innerText = messages[i];
+            const messages = evt.data.split('\n');
+            for (const message of messages) {
+                const item = document.createElement("div");
+                item.innerText = message;
                 appendLog(item);
             }
         };
     } else {
-        var item = document.createElement("div");
+        const item = document.createElement("div");
         item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
         appendLog(item);
     }
